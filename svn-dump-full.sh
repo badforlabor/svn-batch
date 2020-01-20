dc1d08a3006723c29bf5629d4fa78814076db3eb
liubo04 <liubo04@work.com>
2020-01-20 17:39:00 +0800

    1

--- svn-dump-full.sh
+++ svn-dump-full.sh
@@ -4,18 +4,21 @@ SVN_HOME=/usr/bin/
 SVN_ADMIN=$SVN_HOME/svnadmin  
 SVN_LOOK=$SVN_HOME/svnlook
 
-SVN_REPOROOT=/data/svnroot/repository                    
+SVN_REPOROOT=/data/svnroot                    
 
 #backup file path
 date=$(date '+%Y-%m-%d')  
-RAR_STORE=/data/svnbackup/full/$date  
+#RAR_STORE=/data/svnbackup/full/$date  
+RAR_STORE=/data/svnbackup/full
 if [ ! -d "$RAR_STORE" ];then  
 mkdir -p $RAR_STORE  
 fi
 
 cd $SVN_REPOROOT  
 #Projectname 指库名
-for name in $(ls|grep Projectname)  
+for name in $(ls)  
+if [ -d "$name" ]; then
 do  
 $SVN_ADMIN dump $SVN_REPOROOT/$name > $RAR_STORE/full.$name.bak
+fi
 done  
--- svn-dump-incremental.sh
+++ svn-dump-incremental.sh
@@ -4,10 +4,10 @@ SVN_HOME=/usr/bin/
 SVN_ADMIN=$SVN_HOME/svnadmin  
 SVN_LOOK=$SVN_HOME/svnlook
 
-SVN_REPOROOT=/data/svnroot/repository
+SVN_REPOROOT=/data/svnroot
 
 #backup file path
-#date=$(date '+%Y-%m-%d')  
+date=$(date '+%Y-%m-%d')  
 #RAR_STORE=/data/svnbackup/incremental/$date  
 RAR_STORE=/data/svnbackup/incremental
 if [ ! -d "$RAR_STORE" ];then  
@@ -23,17 +23,19 @@ fi
 #read repo list
 cd $SVN_REPOROOT  
 #Projectname 指库名
-for name in $(ls|grep Projectname)  
-do  
-if [ ! -d "$RAR_STORE/$name" ];then  
-mkdir $RAR_STORE/$name  
-fi
+for name in $(ls) 
+#if [ -d "$name" ]; then
+
+#do  
+#if [ ! -d "$RAR_STORE/$name" ];then  
+#mkdir $RAR_STORE/$name  
+#fi
 
 cd $RAR_STORE/$name  
 if [ ! -d "$Log_PATH/$name" ];then  
 mkdir $Log_PATH/$name  
 fi
-
+echo $name
 echo ******Starting backup from $date****** >> $Log_PATH/$name/$name.log  
 echo ******svn repository $name startting to backup****** >> $Log_PATH/$name/$name.log  
 $SVN_LOOK youngest $SVN_REPOROOT/$name > $Log_PATH/A.TMP
@@ -42,9 +44,13 @@ UPPER=`head -1 $Log_PATH/A.TMP`
 NUM_LOWER=`head -1 $Log_PATH/$name/last_revision.txt`  
 let LOWER="$NUM_LOWER+1"
 
-$SVN_ADMIN dump $SVN_REPOROOT/$name -r $LOWER:$UPPER --incremental > $RAR_STORE/$name/$LOWER-$UPPER.dump
+if [ $UPPER -ge $LOWER ]; then
+$SVN_ADMIN dump $SVN_REPOROOT/$name -r $LOWER:$UPPER --incremental > $RAR_STORE/incremental.$name.bak.dump
 rm -f $Log_PATH/A.TMP  
 echo $UPPER > $Log_PATH/$name/last_revision.txt  
 echo ******This time we bakcup from $LOWER to $UPPER****** >> $Log_PATH/$name/$name.log  
 echo ******Back up ended****** >> $Log_PATH/$name/$name.log  
+fi
+
+#fi
 done 
